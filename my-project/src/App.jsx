629e95ba3c484dea04550674e9d09ddf279bcf99
ainul <ainulmufidh@gmail.com>
2024-06-29 10:34:34 +0700

    add content

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import Header from './Header.jsx'
 import Hero from './Hero.jsx'
+import Mid from './Mid.jsx'
+import Review from './Review.jsx'
+
 
 function App() {
 
   return (
     <>
-      {/* <Header /> */}
-      <Hero />
+      <Header />
+      <Hero />   
+      <Review />  
+      <Mid />   
     </>
   )
 }
